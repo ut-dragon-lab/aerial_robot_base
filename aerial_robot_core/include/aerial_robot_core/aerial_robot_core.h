71391d711fefba86cdd554dc8e24fcd25d031c8f
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-04-16 22:05:03 +0900

    [Clang-Format] Run clang-format for all .cpp and .h files.

--- aerial_robot_core/include/aerial_robot_core/aerial_robot_core.h
+++ aerial_robot_core/include/aerial_robot_core/aerial_robot_core.h
@@ -3,16 +3,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
-class AerialRobotCore : public rclcpp::Node
-{
-public:
+class AerialRobotCore : public rclcpp::Node {
+ public:
   AerialRobotCore();
   ~AerialRobotCore();
 
-private:
+ private:
   void mainFunc();
   rclcpp::TimerBase::SharedPtr main_timer_;
 
-  //for debug
+  // for debug
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr debug_pub_;
 };
